f2ce0c41c2695f5e5a7af5cd19c0fee9ef9c1852
Chloe <chloejchou@gmail.com>
2017-03-14 19:41:18 -0700

    add home index pictures to cloudinary

--- frontend/components/home/home_index.jsx
+++ frontend/components/home/home_index.jsx
@@ -5,19 +5,19 @@ class HomeIndex extends React.Component {
   render() {
 
     const popularTags = {
-      "Bakeries": "https://source.unsplash.com/AumHYHRn2AM/",
-      "Cafes": "https://source.unsplash.com/gH5BrIGSPyU/",
-      "Asian Fusion": "https://source.unsplash.com/LDxq4MnYB5U/",
-      "Desserts": "https://source.unsplash.com/MkFTAO4lROs/",
-      "Vegan": "https://source.unsplash.com/oFljzK61O1s/",
-      "Italian": "https://source.unsplash.com/12eHC6FxPyg/",
-      "American": "https://source.unsplash.com/E6HjQaB7UEA/",
-      "Sandwiches": "https://source.unsplash.com/Gg5-K-mJwuQ/",
-      "Breakfast & Brunch": "https://source.unsplash.com/y3aP9oo9Pjc/",
-      "Breweries": "https://source.unsplash.com/MYxvETrYabg/",
-      "Seafood": "https://source.unsplash.com/qvIrI4ueqzY/",
-      "Mexican": "https://source.unsplash.com/aZOqcEK2KuQ/",
-      "Food Trucks": "https://source.unsplash.com/o58cEDAnPB8/"
+      "Bakeries": "http://res.cloudinary.com/dtwr3pge0/image/upload/v1489545266/ben-garratt-134774-min_zgcsfw.jpg",
+      "Cafes": "http://res.cloudinary.com/dtwr3pge0/image/upload/v1489545269/tadas-mikuckis-45432-min_bsmcte.jpg",
+      "Asian Fusion": "http://res.cloudinary.com/dtwr3pge0/image/upload/v1489545266/artur-rutkowski-61567-min_em0w5c.jpg",
+      "Desserts": "http://res.cloudinary.com/dtwr3pge0/image/upload/v1489545268/igor-ovsyannykov-152531-min_suddyq.jpg",
+      "Vegan": "http://res.cloudinary.com/dtwr3pge0/image/upload/v1489545266/adam-jaime-119568-min_bcbtuz.jpg",
+      "Italian": "http://res.cloudinary.com/dtwr3pge0/image/upload/v1489545267/eaters-collective-132773-min_dfebej.jpg",
+      "American": "http://res.cloudinary.com/dtwr3pge0/image/upload/v1489545267/dan-gold-105699-min_pubzg8.jpg",
+      "Sandwiches": "http://res.cloudinary.com/dtwr3pge0/image/upload/v1489545268/eaters-collective-172257-min_ebt0ez.jpg",
+      "Breakfast & Brunch": "http://res.cloudinary.com/dtwr3pge0/image/upload/v1489545267/ali-inay-9858-min_egzwpf.jpg",
+      "Breweries": "http://res.cloudinary.com/dtwr3pge0/image/upload/v1489545269/pawel-kadysz-90225-min_jowlcs.jpg",
+      "Seafood": "http://res.cloudinary.com/dtwr3pge0/image/upload/v1489545266/adrien-sala-46727-min_lrb6yv.jpg",
+      "Mexican": "http://res.cloudinary.com/dtwr3pge0/image/upload/v1489545268/herson-rodriguez-96102-min_bqv1jo.jpg",
+      "Food Trucks": "http://res.cloudinary.com/dtwr3pge0/image/upload/v1489545268/filip-sablatura-685-min_iruujo.jpg"
     };
 
     return(
