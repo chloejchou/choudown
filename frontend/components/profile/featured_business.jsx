14838f123d908a87be9cde0deb0ec8ad58283d31
Chloe Chou <chloe@gladly.com>
2018-02-09 15:50:53 -0800

    featured business changes

--- frontend/components/profile/featured_business.jsx
+++ frontend/components/profile/featured_business.jsx
@@ -29,40 +29,39 @@ class FeaturedBusiness extends React.Component {
       return <Loading />;
     }
 
-    return(
+    return (
       <div id="featured-business">
-        <h2>{"Featured Business*"}</h2>
+        <h2>{'Featured Business*'}</h2>
         <h3>‘āina</h3>
         <h4>" āina is a Hawaiian word meaning the land which feeds us "</h4>
         <div>
           <div className="col col-1-2">
             <p>
-              Aloha! ‘āina is a modern Hawaiian food concept that serves food
-              with a distinct Hawaiian influence with a breath of the classics.
-              Everything we serve is from the land and rooted in what the land
-              has given to us.
+              Aloha! ‘āina is a modern Hawaiian food concept that serves food with a distinct Hawaiian influence with a
+              breath of the classics. Everything we serve is from the land and rooted in what the land has given to us.
             </p>
-            <img src="https://static1.squarespace.com/static/561c7ed1e4b0ec370dd173f9/t/58006dba2994ca4ae4e9baa9/1460441363406/aina-1010595.jpg?format=1500w"></img>
+            <img src="http://res.cloudinary.com/dtwr3pge0/image/upload/v1518217970/aina_Brunch_spread_by_Allie.Eats_March_2017_Highres_qmh7oz.jpg" />
             <p>
-              We cook with the seasons and with love, using
-              fine dining techniques and local ingredients from the Bay Area
-              as well as specially imported ingredients from Hawai'i.
+              We cook with the seasons and with love, using fine dining techniques and local ingredients from the Bay
+              Area as well as specially imported ingredients from Hawai'i.
             </p>
-            <img src="https://static1.squarespace.com/static/561c7ed1e4b0ec370dd173f9/t/5800f39420099e26aedb3726/1460437297634/'aina-mdx-85-fave.jpg?format=2500w"></img>
+            <img src="http://res.cloudinary.com/dtwr3pge0/image/upload/v1518218163/aina-9_POKE_by_Bay_Area_Food_Critics_d2vfeu.jpg" />
           </div>
           <div className="col col-1-2">
-            <img src="https://static1.squarespace.com/static/561c7ed1e4b0ec370dd173f9/56e6f97d8259b5f1d57a9a81/571548ffe32140294d95dc5a/1461217398702/'aina-mdx-333-fave.jpg"></img>
+            <img src="http://res.cloudinary.com/dtwr3pge0/image/upload/v1518217970/aina_Octopus_Luau_Bay_Area_Food_Critics_vdlpmk.jpg" />
             <p>
-              The ‘āina concept strives to be a love letter to the islands,
-              by merging the attraction of Hawaiian-inspired cuisine,
-              the warmth and hospitality of the island spirit with a refined
-              sensibility and great service in a casual and welcoming atmosphere.
+              The ‘āina concept strives to be a love letter to the islands, by merging the attraction of
+              Hawaiian-inspired cuisine, the warmth and hospitality of the island spirit with a refined sensibility and
+              great service in a casual and welcoming atmosphere.
             </p>
-            <img src="https://static1.squarespace.com/static/561c7ed1e4b0ec370dd173f9/56e6f97d8259b5f1d57a9a81/571546fa60b5e99b8bc0e03b/1461217398608/'aina-mdx-171-fave.jpg"></img>
+            <img src="http://res.cloudinary.com/dtwr3pge0/image/upload/v1518218321/aina_FOOD_Highres-5_Guava_Malasadas_u2ovof.jpg" />
           </div>
         </div>
-        <button onClick={this.redirectToBusiness}>check our their reviews</button><br />
-        <a href="http://www.ainasf.com/">{"*all information is property of the"} <strong>‘āina website</strong></a>
+        <button onClick={this.redirectToBusiness}>check our their reviews</button>
+        <br />
+        <a href="http://www.ainasf.com/">
+          {'*all information is property of the'} <strong>‘āina website</strong>
+        </a>
       </div>
     );
   }
